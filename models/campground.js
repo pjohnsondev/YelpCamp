cb740a01e3ce3234130b3868e17237ea2c0413e7
pjohnsondev <pjohnsondev@gmail.com>
2020-07-12 19:53:51 +1000

    Adds time since created to camgrounds and comments

--- models/campground.js
+++ models/campground.js
@@ -7,6 +7,10 @@ var campgroundSchema = new mongoose.Schema({
     price: String,
     image: String,
     description: String,
+    location: String,
+    lat: Number,
+    lng: Number,
+    createdAt: {type: Date, default: Date.now},
     author: {
         id: {
             type: mongoose.Schema.Types.ObjectId,
